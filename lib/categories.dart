e4db5b24b093414c170d9b937ba2333bab5af773
yjf <505978219@qq.com>
2025-07-22 11:40:42 +0800

    🎉 init(Init): project Init

---
+++ lib/categories.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// 无状态组件
+class CategoriesScreen extends StatelessWidget {
+  // 这是主页的分类页面
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick your category')),
+      body: GridView(
+        // 设置网格的列数
+        // SliverGridDelegateWithFixedCrossAxisCount 是用来设置网格的列数
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        // 设置网格的子元素
+        children: const [
+          // CategoryItem(title: 'Italian', color: Colors.red),
+          // CategoryItem(title: 'Italian', color: Colors.red),
+          Text('Italian', style: TextStyle(color: Colors.white)),
+          Text('Italian', style: TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
